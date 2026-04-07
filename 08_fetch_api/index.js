0d3e922bf7765a2781579c59c206c1b2892baae0
Bertzooka <bertramrogerterndrup@gmail.com>
2026-04-07 09:59:32 +0200

    blackjack

--- 08_fetch_api/index.js
+++ 08_fetch_api/index.js
@@ -1,61 +1,47 @@
 var currentPage = '#page1'
 var deck
+
 var player = {
     cards: [],
-    total:0
+    total: 0
 }
 
 var dealer = {
     cards: [],
-    total:0
+    total: 0
 }
 
 var state = "begin"
 
-//P5 setup() bliver kaldt en gang føre siden vises
+// P5 setup() bliver kaldt EN gang før siden vises 
 function setup() {
     console.log('P5 setup kaldt')
 
-    //skift til current page
     shiftPage(currentPage)
-
-    //fetch er en asynkron funktion som kan hente data ude i byen
-
     getDeck()
 
-    select('#getCardBtn').mousePressed(()=>{
-        getCard()
-    })
+    select('#playerDrawBtn').mousePressed(() => drawCard("player"))
+    select('#playerStandBtn').mousePressed(() => drawCard("dealer"))
+    select('#restartBtn').mousePressed(restart)
 
-    //Sæt menu op
-    // hent alle sider som et array
     var allPages = selectAll('.page')
-    // Løb listen igennem en for en
     allPages.map(
         page => {
-            // Lav et nyt <a> element 
             var menuItem = createElement('a')
             menuItem.html(page.attribute('title'))
-            // sæt eventlistener på a tagget
             menuItem.mousePressed(
                 () => shiftPage('#' + page.attribute('id'))
             )
-            // sæt a tagget ind i sidebaren
             select('.sidebar').child(menuItem)
-
         }
     )
 }
-//Async står for asyncronous - vi ved ikke præcist hvor længe det tager at køre funktionen
+
 async function getDeck() {
     try {
-        //Fetch kan hente data fra en server ude i byen
-        const respone = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-        //response objektet kommer tilbage fra serveren - og HVIS response.ok er true, kan vi hente data
-        console.log("Response objektet:", response)
+        const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
         if (response.ok) {
             const data = await response.json()
-            console.log("Data vi får tilbage:", data)
             deck = data
             drawCard()
         }
@@ -64,46 +50,154 @@ async function getDeck() {
     }
 }
 
-function drawCard(){
-    if(state == "begin"){
-        var cardOne = getOneCard()
-        player.cards.push(cardOne)
-        var cardTwo = getOneCard()
-        player.cards.push(cardTwo)
-        var dealerCardOne = getOneCard()
-        dealer.cards.push(dealerCardOne)
-        var dealerCardTwo = getOneCard()
-        dealer.cards.push(dealerCardTwo)
+async function drawCard(newState) {
+    if (newState) {
+        state = newState
+    }
+
+    // 1. Spilleren taber (Bust)
+    if (state == "playerLose") {
+        select('#result').html("You went bust! Dealer wins.")
+        shiftPage('#page2')
+        return
+    }
+
+    // 2. Dealerens tur og 3. Afgør vinderen
+    if (state == "dealer") {
+        // Vis skjult kort
+        dealer.cards[0].hidden = false
         showCards()
-    }
-    if(state == "dealer"){
 
-    }
-    if(state = "player"){
+        // Dealer trækker kort indtil summen er mindst 17
+        while (dealer.total < 17) {
+            var newCard = await getOneCard()
+            dealer.cards.push(newCard)
+            dealer.total += returnCardValue(newCard)
+            
+            // Tjek for es-logik for dealeren også
+            if (dealer.total > 21) {
+                dealer.cards.forEach(c => {
+                    if (c.value === "ACE") {
+                        c.value = "ACE-USED"
+                        dealer.total -= 10
+                    }
+                })
+            }
+            showCards()
+        }
 
+        // Afgør vinderen
+        let message = ""
+        if (dealer.total > 21) {
+            message = "Dealer busts! You win!"
+        } else if (player.total > dealer.total) {
+            message = "You win!"
+        } else if (dealer.total > player.total) {
+            message = "Dealer wins!"
+        } else {
+            message = "It's a draw!"
+        }
+
+        select('#result').html(message)
+        shiftPage('#page2')
+        return
+    }
+
+    if (state == "player") {
+        var newCard = await getOneCard()
+        player.cards.push(newCard)
+        player.total += returnCardValue(newCard)
+
+        // 4. Ret es-logikken (Spiller)
+        if (player.total > 21) {
+            player.cards.forEach(c => {
+                if (c.value === "ACE" && player.total > 21) {
+                    c.value = "ACE-USED"
+                    player.total -= 10
+                }
+            })
+        }
+
+        showCards()
+
+        if (player.total > 21) {
+            state = "playerLose"
+            drawCard()
+        } else if (player.total == 21) {
+            state = "dealer"
+            drawCard()
+        }
+    }
+
+    if (state == "begin") {
+        // Træk startkort
+        var p1 = await getOneCard()
+        var p2 = await getOneCard()
+        player.cards.push(p1, p2)
+        player.total = returnCardValue(p1) + returnCardValue(p2)
+
+        var d1 = await getOneCard()
+        var d2 = await getOneCard()
+        d1.hidden = true
+        dealer.cards.push(d1, d2)
+        dealer.total = returnCardValue(d1) + returnCardValue(d2)
+
+        // Tjek for start-Blackjack
+        if (dealer.total == 21 || player.total == 21) {
+            state = "dealer"
+            drawCard()
+        } else {
+            state = "player"
+            showCards()
+        }
     }
 }
 
-function showCards(){
-    console.log("Show cards er klar med" +  player.cards, dealer.cards)
+function restart() {
+    player.cards = []
+    player.total = 0
+    dealer.cards = []
+    dealer.total = 0
+    state = "begin"
+    shiftPage('#page1')
+    drawCard()
+}
+
+function showCards() {
     select('#player .cards').html('')
-    player.cards.map( (c, i) => {
+    player.cards.map((c, i) => {
         var img = createImg(c.image)
-        img.style('transform', `translate(${i*40}px), ${i*40}px)`)
+        img.style('transform', `translate(${i * 40}px, ${i * 40}px)`)
         select('#player .cards').child(img)
+    })
+    
+    select('#dealer .cards').html('')
+    dealer.cards.map((c, i) => {
+        var img = (c.hidden) ? createImg('https://deckofcardsapi.com/static/img/back.png') : createImg(c.image)
+        img.style('transform', `translate(${i * 40}px, ${i * 40}px)`)
+        select('#dealer .cards').child(img)
     })
 }
 
-async function getOneCard(){
-        //Hent et kort
-    var card
-    try{
+function returnCardValue(card) {
+    if (isNaN(card.value)) {
+        if (card.value == "ACE" || card.value == "ACE-USED") {
+            return 11
+        } else {
+            return 10
+        }
+    } else {
+        return Number(card.value)
+    }
+}
+
+async function getOneCard() {
+    try {
         const response = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`)
         const data = await response.json()
-        console.log("DrawCard kommer tilbage med et nyt kort" + data)
         return data.cards[0]
-    } catch(error){
-        console.log("Error catched", error)
+    } catch (error) {
+        console.log("Error", error)
     }
 }
 
